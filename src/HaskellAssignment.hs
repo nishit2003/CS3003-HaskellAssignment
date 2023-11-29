34a68ec348fa845911fa4a49c25fe1ad96fe6259
Nishit Grover <grovernt@mail.uc.edu>
2023-11-29 13:53:19 -0500

    final commit

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -11,20 +11,20 @@ instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
-findFirst _ [] = NoMatch
-findFirst p xs@(x:xs') |
-p x       = Match $ length xs - length xs'
-| otherwise = findFirst p xs'
 
-
+findFirst needle haystack@(x : xs) -- takes 2 parameter, needle(to "find" the item of interest) and haystack(list)
+  | needle x = Match 0         -- If the first element satisfies the predicate, return 'Match' at index 0
+  | otherwise = case findFirst needle xs of  --If the first element does not satisfy the predicate, recursively search in the remaining elements
+                  NoMatch -> NoMatch  -- If no match is found in the remaining elements, return 'NoMatch'
+                  Match index -> Match (index + 1)  -- If a match is found, return 'Match' at the next index
+findFirst _ [] = NoMatch --If the list is empty, return 'NoMatch'
 
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
-palindrome [] = True
-palindrome str@(x:xs)
-| str == reverse str = True
-| otherwise          = False
-where
-  revStr = reverse str
+palindrome [] = True --empty string is True
+palindrome str = str == reverse str --reverse str reverses the list, and str == reverse str checks if the original list is equal to its reverse, indicating that it's a palindrome.
+  
+
+
